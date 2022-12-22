d16c4b2d04e34cebf635493178095231470b1efc
techking5287 <techking5287@gmail.com>
2022-12-21 18:43:49 -0800

    add graphql files

---
+++ src/@graphql/mutations.js
@@ -0,0 +1,37 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_MUTATION = gql`
+	mutation Login ($email: String $password: String!) {
+		tokenAuth(
+			email: $email
+			password: $password
+		){
+			token
+			errors
+			user{
+				username
+				email
+			}
+		}
+  	}
+`
+
+export const REGISTER_MUTATION = gql`
+	mutation register(
+		$username: String!
+		$password1: String!
+		$password2: String!
+		$email: String!
+	) {
+		register(
+			username: $username
+			password1: $password1
+			password2: $password2
+			email: $email
+		) {
+			success
+			errors
+			token
+		}
+	}
+`
