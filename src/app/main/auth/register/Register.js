590e0664580c1ec0b4561cad461f0c2816acff43
techking5287 <techking5287@gmail.com>
2022-12-21 17:41:49 -0800

    change register format

--- src/app/main/auth/register/Register.js
+++ src/app/main/auth/register/Register.js
@@ -9,12 +9,17 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import JWTRegisterTab from './JWTRegisterTab';
 
+import logo from "@assets/images/logos/fuse.svg";
+import darkBackground from "@assets/images/backgrounds/dark-material-bg.jpg";
+
+
 const useStyles = makeStyles((theme) => ({
   root: {
-    background: `linear-gradient(to right, ${theme.palette.primary.dark} 0%, ${darken(
-      theme.palette.primary.dark,
-      0.5
-    )} 100%)`,
+    // background: `linear-gradient(to right, ${theme.palette.primary.dark} 0%, ${darken(
+    //   theme.palette.primary.dark,
+    //   0.5
+    // )} 100%)`,
+    background: `url(${darkBackground})`,
     color: theme.palette.primary.contrastText,
   },
   leftSection: {},
@@ -39,36 +44,37 @@ function Register() {
     <div
       className={clsx(
         classes.root,
-        'flex flex-col flex-auto items-center justify-center flex-shrink-0 p-16 md:p-24'
+        "flex flex-col flex-auto items-center justify-center flex-shrink-0 p-16 md:p-24"
       )}
     >
       <motion.div
         initial={{ opacity: 0, scale: 0.6 }}
         animate={{ opacity: 1, scale: 1 }}
-        className="flex w-full max-w-400 rounded-20 shadow-2xl overflow-hidden"
+        className="flex w-full max-w-400 md:max-w-3xl rounded-20 shadow-2xl overflow-hidden"
       >
         <Card
           className={clsx(
             classes.leftSection,
-            'flex flex-col w-full max-w-sm items-center justify-center shadow-0'
+            "flex flex-col w-full max-w-sm items-center justify-center shadow-0"
           )}
           square
         >
-          <CardContent className="">
+          <CardContent className="flex flex-col items-center justify-center w-full py-96 max-w-320">
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1, transition: { delay: 0.2 } }}
-              className="flex items-center justify-center"
             >
-              <img className="logo-icon px-36 w-full" src="assets/logos/mirador.png" alt="logo" />
+              <div className="flex items-center mb-12">
+                <img className="logo-icon w-full" src={logo} alt="logo" />
+              </div>
             </motion.div>
           </CardContent>
 
           <div className="flex flex-col items-center justify-center pb-32">
             <div>
-              <span className="font-normal mr-8">Already have an account?</span>
-              <Link className="font-normal" to="/login">
-                Login...
+              <span className="font-normal mr-8">Don't have an account?</span>
+              <Link className="font-normal" to="/register">
+                Register
               </Link>
             </div>
             {/* <Link className="font-normal mt-8" to="/">
@@ -76,7 +82,52 @@ function Register() {
             </Link> */}
           </div>
           <JWTRegisterTab />
+          <div className="flex flex-col items-center justify-center pb-32">
+            <div>
+              <span className="font-normal mr-8">Lost Your Password?</span>
+              <Link className="font-normal" to="/forgetPassword">
+                Forget Password...
+              </Link>
+            </div>
+          </div>
         </Card>
+
+        <div
+          className={clsx(
+            classes.rightSection,
+            "hidden md:flex flex-1 items-center justify-center p-64"
+          )}
+        >
+          <div className="max-w-320">
+            <motion.div
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
+            >
+              <Typography
+                variant="h5"
+                color="inherit"
+                className="text-center font-semibold leading-tight"
+              >
+                GUARANTEED FUNDING
+              </Typography>
+              <Typography
+                variant="h5"
+                color="inherit"
+                className="text-center font-semibold leading-tight"
+              >
+                AND PAYOUTS
+              </Typography>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, transition: { delay: 0.3 } }}
+            >
+              <Typography variant="body1" color="inherit" className="text-center mt-32">
+                {/* Keep 90% of the profits. */}
+              </Typography>
+            </motion.div>
+          </div>
+        </div>
       </motion.div>
     </div>
   );
