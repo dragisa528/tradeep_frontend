c4c63a540117f5c65b045aa2335c0f1f971f2835
techking5287 <techking5287@gmail.com>
2023-01-23 16:17:09 -0800

    change logo

--- src/app/fuse-layouts/shared-components/Logo.js
+++ src/app/fuse-layouts/shared-components/Logo.js
@@ -32,17 +32,8 @@ function Logo() {
     <div className={clsx(classes.root, 'flex items-center')}>
       <img className="logo-icon w-24 h-24" src={fuse} alt="logo" />
       <Typography className="text-16 leading-none mx-12 font-medium logo-text" color="inherit">
-        FUSE
+        DEEPTRADE
       </Typography>
-      <div className={clsx(classes.reactBadge, 'react-badge flex items-center py-4 px-8 rounded')}>
-        <img
-          className="react-logo"
-          src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
-          alt="react"
-          width="16"
-        />
-        <span className="react-text text-12 mx-4">React</span>
-      </div>
     </div>
   );
 }
